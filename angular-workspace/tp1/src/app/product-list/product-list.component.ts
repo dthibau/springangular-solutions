f1af8809714060420d30853b55a3bba659493b55
David THIBAU <david.thibau@gmail.com>
2021-02-04 19:09:28 +0100

    Injection du FakeService

--- angular-workspace/tp1/src/app/product-list/product-list.component.ts
+++ angular-workspace/tp1/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../service/product-service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,24 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  public name: string;
-  public style: string;
-  public disabled: boolean;
-  public products: Array<any> = new Array();
 
-  constructor() { 
-    this.name = 'Hello world !!';
-    this.style = "red";
-    this.disabled = true;
+  public products: Array<any> = [];
+
+  constructor(private productService: ProductService) { 
   }
 
   ngOnInit(): void {
-    this.products.push({date: '25/09/2020', reference :'pro1'});
-    this.products.push({date: '21/03/2020', reference :'myr2'});
+    this.products = this.productService.findAll();
   
   }
 
-  toggleButton() {
-    this.disabled = ! this.disabled;
-  }
+
 }
