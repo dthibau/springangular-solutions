610883c550a906468d243d7bf3fe5dfaef2f0016
David THIBAU <david.thibau@gmail.com>
2021-02-04 11:36:14 +0100

    Interpolation, Binding, Directives, Variables locales, Binding bi-directionel

--- angular-workspace/tp1/src/app/product-list/product-list.component.ts
+++ angular-workspace/tp1/src/app/product-list/product-list.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  public name: string;
+  public style: string;
+  public disabled: boolean;
+  public products: Array<any> = new Array();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    this.name = 'Hello world !!';
+    this.style = "red";
+    this.disabled = true;
   }
 
+  ngOnInit(): void {
+    this.products.push({date: '25/09/2020', reference :'pro1'});
+    this.products.push({date: '21/03/2020', reference :'myr2'});
+  
+  }
+
+  toggleButton() {
+    this.disabled = ! this.disabled;
+  }
 }
