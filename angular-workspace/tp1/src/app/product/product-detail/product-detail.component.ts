4daa8178908e7af56216b293f235b8dccda02f3f
David THIBAU <david.thibau@gmail.com>
2021-02-05 14:39:30 +0100

    Formulaire et création nouveau produit

--- angular-workspace/tp1/src/app/product/product-detail/product-detail.component.ts
+++ angular-workspace/tp1/src/app/product/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product-service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  private product: Product = new Product();
+  productForm!: FormGroup;
+
+  constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder, 
+    private productService: ProductService) { 
+    
+    }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      const id = params.get('id');
+      if ( id !== null ) {
+        this.product = this.productService.findOne(+id);
+      } else {
+        this.product = new Product();
+      }
+      this.buildForm();
+    });
+    
   }
 
+  save() {
+    let id = this.product.id;
+    this.product = this.productForm.value;
+    if ( id > 0 ) {
+      this.product = this.productService.update(this.product); 
+    } else {
+      this.product = this.productService.create(this.product); 
+    }
+
+    this.router.navigate(['/products/list']);
+  }
+
+  private buildForm() {
+    this.productForm = this.fb.group({
+      reference: [this.product.reference, [Validators.required, Validators.maxLength(5)]],
+      nom: [this.product.nom, [Validators.required, Validators.minLength(3)]],
+      description: [this.product.description],
+      prixUnitaire: [this.product.prixUnitaire, Validators.min(1)]
+    });
+    // Filter les lettres
+    this.productForm.get('prixUnitaire')!.valueChanges.subscribe((newValue) => {
+      console.log("Value changed !");
+      if ( newValue.match(/[a-z]/i) ) {
+        this.productForm.get('prixUnitaire')!.setValue(newValue.replaceAll(/[a-z]/ig,''));
+      }
+    });
+
+  }
 }
