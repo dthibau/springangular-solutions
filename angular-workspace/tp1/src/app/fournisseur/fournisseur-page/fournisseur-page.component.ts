b0e171c3b0fced126c12ba4f242b9f5aab9990ee
David THIBAU <david.thibau@gmail.com>
2021-02-05 11:48:11 +0100

    Routing

---
+++ angular-workspace/tp1/src/app/fournisseur/fournisseur-page/fournisseur-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fournisseur-page',
+  templateUrl: './fournisseur-page.component.html',
+  styleUrls: ['./fournisseur-page.component.css']
+})
+export class FournisseurPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
