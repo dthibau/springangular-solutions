4dfe87fdaa5ed4e65eda37e7ac55223569106879
David THIBAU <david.thibau@gmail.com>
2021-02-05 10:31:26 +0100

    Routing simple

--- angular-workspace/tp1/src/app/app.module.ts
+++ angular-workspace/tp1/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductService } from './service/product-service';
 import { FakeService } from './service/fake-service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { RouterModule } from '@angular/router';
+import { ProductPageComponent } from './product/product-page/product-page.component';
 
 
 
@@ -15,12 +17,18 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
   declarations: [
     AppComponent,
     ProductListComponent,
+    ProductPageComponent,
     NavigationBarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    RouterModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'products', pathMatch: 'full' },
+      { path: 'products', component: ProductPageComponent }
+    ])
   ],
   providers: [
     { provide: ProductService, useClass: FakeService }
