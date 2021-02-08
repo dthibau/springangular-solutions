4f4ca018b62525228714446913df5b4e6ecb688e
David THIBAU <david.thibau@gmail.com>
2021-02-08 12:13:52 +0100

    Atelier 1 Spring

---
+++ spring-workspace/tp1/src/test/java/org/formation/Tp1ApplicationTests.java
@@ -0,0 +1,18 @@
+package org.formation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class Tp1ApplicationTests {
+	
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Test
+	void contextLoads() {
+	}
+
+}
