dee845052385c6f7c07bbdc2c2c8d5e1d537423f
David THIBAU <david.thibau@gmail.com>
2021-02-09 17:19:51 +0100

    Syntaxe template + Output

--- angular-workspace/tp1/src/app/app.component.ts
+++ angular-workspace/tp1/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tp1';
+
+  logEvent(event: any) {
+    console.log(event);
+  }
 }
--- angular-workspace/tp1/src/app/product-list/product-list.component.ts
+++ angular-workspace/tp1/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  name: string;
+  style: string = 'red'
+  disabled: boolean = true
+  products!: Array<any>;
+  @Output() toggleEvent = new EventEmitter<boolean>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    console.log("Constructeur")
+    this.name = "Hello"
   }
 
+  ngOnInit(): void {
+    
+    this.products = [{reference: 'ref1', date:'2021'}, {reference: 'ref2', date:'2020'}]
+    this.products.push({reference: 'ref3', date:'2019'})
+    console.log("Init "+this.products)
+  }
+
+  toggleButton() {
+    console.log("Toggle")
+    this.disabled = !this.disabled
+    this.toggleEvent.emit(this.disabled);
+  }
 }
